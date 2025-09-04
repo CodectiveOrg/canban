aabfc51d40277d2b9674d841881c41d9e22451e3
BijanProgrammer <bijaneisapour@gmail.com>
2025-09-04 08:33:47 +0330

    [ADD] controlled input example

--- src/components/TextInput/TextInput.tsx
+++ src/components/TextInput/TextInput.tsx
@@ -1,4 +1,10 @@
-import { type ComponentProps, type ReactNode, useId } from "react";
+import {
+  type ChangeEvent,
+  type ComponentProps,
+  type ReactNode,
+  useId,
+  useState,
+} from "react";
 
 import clsx from "clsx";
 
@@ -17,12 +23,29 @@ export default function TextInput({
 }: Props): ReactNode {
   const id = useId();
 
+  const [value, setValue] = useState<string>("");
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>): void => {
+    const n = Number.parseInt(e.target.value.replaceAll(/\D/g, ""));
+
+    if (Number.isNaN(n)) {
+      return;
+    }
+
+    setValue(n.toLocaleString());
+  };
+
   return (
     <div
       className={clsx(styles["text-input"], !!error && styles.error, className)}
     >
       <label htmlFor={id}>{label}</label>
-      <input id={id} {...otherProps} />
+      <input
+        id={id}
+        {...otherProps}
+        value={value}
+        onChange={handleInputChange}
+      />
       <span className={styles.error}>{error || "\u00A0"}</span>
     </div>
   );
